1693bc79ee93b645b331e63a8bc587553713aafc
Felix Ackuayi <fackuayi@payswitch.com.gh>
2020-10-22 13:30:03 +0000

    Taken through Variable declaration using (let) and constant variable using (const)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
-alert("This works...");
+
+//alert("This works...");
+//Purpose is to be able to do a calculator by entering a Number and (add,sub,mult,divide) to the last number enter
+//Calculator that works with two numbers
+
+//We need Variable and Constants
+/*Variable: I is a data container or storage where the stored data will be used to achieve a purpose. Or data container where it value can change
+   In JS the format is [let userName = 'Felix';
+                             userName = 'Ackuayi';
+                        ]
+
+     Constant: With this form of variable declaration the value in the variable is fixed and cannot be changed
+                  In JS the format is [const totalUsers = 15;
+                             userName =  20; //this will throw an error because it has been declared constant and cannot be changed
+                        ]
+
+
+*
+* */
