5140ac741d34aaa75ae3946140e12b55363a8eef
Felix Ackuayi <fackuayi@payswitch.com.gh>
2020-10-22 20:38:20 +0000

    Assignment quiz answered

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,36 @@
+let user = 10;
+
+let result;
+
+result = 18;
+
+let addition;
+
+let sub;
+
+let divide;
+
+ addition = result + user;
+ sub = result - user;
+ divide = result/user;
+
+ alert(addition);
+alert(sub);
+alert(divide);
+
+
+
+/******************************************INSTRUCTOR SOLUTION*************************************/
+
+const userInput = 20;
+let results;
+
+results = 18 + userInput;
+
+results = results - 10;
+results = results * 2;
+results = results / 4;
+
+alert(results);
+alert(userInput);
+/*******************************************i**************************************/
